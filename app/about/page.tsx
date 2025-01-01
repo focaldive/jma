0bb68240586067f5a327f8c4fec9aa94608c4e98
CodeforGhost <ilikeabdul7@gmail.com>
2025-01-01 19:43:58 +0530

    add About page and hero section components

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AboutPage from "../components/about/about-hero-section";
+
+const About = () => {
+  return (
+    <main>
+      <AboutPage />
+    </main>
+  );
+};
+
+export default About;
