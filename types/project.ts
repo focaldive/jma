d9cd25577d294b5e05e6d60b2eeaa7d1f06a6031
CodeforGhost <ilikeabdul7@gmail.com>
2025-01-01 19:46:15 +0530

    add Project and ProjectCategory interfaces for project management

---
+++ types/project.ts
@@ -0,0 +1,15 @@
+export interface Project {
+    year: number;
+    month: string;
+    description: string;
+    location: string;
+    currency: string;
+    amount: number;
+}
+
+export interface ProjectCategory {
+    name: string;
+    value: number;
+    color: string;
+}
+
