ffbb80605f1ad356107f3b39db8e339787705c66
CodeforGhost <ilikeabdul7@gmail.com>
2025-01-02 20:52:26 +0530

    add donation page component and main entry for donations

---
+++ app/donate/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DonationPage from "../components/donate/donate-page";
+
+const Donate = () => {
+  return (
+    <main>
+      <DonationPage />
+    </main>
+  );
+};
+
+export default Donate;
