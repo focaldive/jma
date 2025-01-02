7a68df46fa28ab8668b665b4da7d4fbb1279ede6
CodeforGhost <ilikeabdul7@gmail.com>
2025-01-02 15:57:21 +0530

    add YouTube image domain to Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["images.unsplash.com", "jaffnamuslimuk.org", "media.istockphoto.com"],
+    domains: ["images.unsplash.com", "jaffnamuslimuk.org", "media.istockphoto.com", "img.youtube.com"],
   },
 
 };
